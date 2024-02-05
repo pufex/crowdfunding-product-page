554dd2617dacb1a025b5b7dfbb6be0e383e7cbcf
Ajarek <ajarek@poczta.onet.pl>
2024-02-05 17:04:44 +0100

    changes

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const radioButtons = document.querySelectorAll(".the-input");
 const pledgeChoices = document.querySelectorAll(".pledge-choice");
 const choiceWindows = document.querySelectorAll(".choice-window");
+const gotIt = document.querySelector("#got-it");
+const pledgeCompleted = document.querySelector(".pledge-completed");
+const selectReward = document.querySelectorAll(".select-reward");
+const enterPledges = document.querySelectorAll(".enter-pledge");
+const choiceWindow = document.querySelector(".choice-window");
+
+console.log(choiceWindow);
 
 let i = 0;
 
@@ -25,3 +32,35 @@ choiceWindows.forEach((choiceWindow) => {
   i++;
 })
 
+gotIt.addEventListener('click', () => {
+  pledgeCompleted.classList.toggle('hidden');
+})
+
+selectReward.forEach((item) => {
+  item.addEventListener('click', () => {
+    choiceWindows[0].classList.toggle('hidden');
+    console.log(choiceWindows[0].classList);
+  })
+})
+
+
+console.log(enterPledges)
+enterPledges.forEach((enterPledge, index) => {
+  let pledgeBar = [25, 75, 200, 999999];
+  const continueButton = enterPledge.querySelector(".continue");
+  const cashInput = enterPledge.querySelector(".cash-input");
+  const label = enterPledge.querySelector("label")
+  continueButton.addEventListener('click', () => {
+    cashInput.classList.remove("error-input")
+    label.classList.remove("error-label")
+    label.innerHTML = "Enter your pledge"
+    if(cashInput.value >= pledgeBar[index]) {
+      choiceWindow.classList.toggle("hidden");
+      pledgeCompleted.classList.toggle("hidden");
+    }else{
+      cashInput.classList.add("error-input")
+      label.classList.add("error-label")  
+      label.innerHTML = "Enter a valid pledge"
+    }
+  })
+})
